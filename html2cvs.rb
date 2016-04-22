db7eca63e443890a1a1489d2619a1442dc2564f3
Harry Hartmann <hry@solaris-it.com>
2016-04-22 18:49:00 -0400

    first commit

---
+++ html2cvs.rb
@@ -0,0 +1,18 @@
+#!//usr/bin/env ruby
+
+require 'rubygems'
+require 'open-uri'
+require 'nokogiri'
+require 'csv'
+
+### document = Nokogiri::HTML(input)
+### document = File.open("invoices.html") { |f| Nokogiri::XML(f) }
+document = Nokogiri::HTML(open("http://www.itl.nist.gov/div898/handbook/eda/section3/eda3672.htm")) { |f| Nokogiri::XML(f) }
+
+document.at('table').search('tr').each do |row|
+  cells = row.search('th, td').map { |cell| cell.text.strip }
+
+  puts CSV.generate_line(cells)
+end
+
+puts "hello !!!!!"
