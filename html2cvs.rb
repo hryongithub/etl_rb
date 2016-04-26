398ac95f46fa68ee0d90b4cf6c6f50f814e2a433
Harry Hartmann <hry@solaris-it.com>
2016-04-26 15:07:19 -0400

    message afret iss#12

--- html2cvs.rb
+++ html2cvs.rb
@@ -8,7 +8,8 @@ require 'csv'
 ### document = Nokogiri::HTML(input)
 ### document = File.open("invoices.html") { |f| Nokogiri::XML(f) }
 ### document = Nokogiri::HTML(open("http://www.itl.nist.gov/div898/handbook/eda/section3/eda3672.htm")) { |f| Nokogiri::XML(f) }
-document = Nokogiri::HTML(open("http://aws-t2/C/invoices.html")) { |f| Nokogiri::XML(f) }
+### document = Nokogiri::HTML(open("http://aws-t2/C/invoices.html")) { |f| Nokogiri::XML(f) }
+document = File.open("invoices.html") { |f| Nokogiri::XML(f) }
 
 document.at('table').search('tr').each do |row|
   cells = row.search('th, td').map { |cell| cell.text.strip }
